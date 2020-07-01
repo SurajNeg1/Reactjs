334e79c585003d043f0bc5a014d4471f2e942b73
judgesupreme <1234surajsinghnegi@gmail.com>
2020-07-01 23:36:08 +0530

    Uncontrolled Form nd Login Modal

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -66,8 +66,8 @@ class Contact extends Component {
             errors.lastname = 'Last Name should be >=3 characters';
         else if(this.state.touched.lastname && lastname.length > 10)
             errors.lastname = 'Last Name should be <=10 characters';
+        
         const reg=/^\d+$/;
-       
         if(this.state.touched.telnum && !reg.test(telnum))
             errors.telnum = 'Tel. Num should contain only numbers';
         
