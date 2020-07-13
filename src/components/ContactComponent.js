a62f6783389fd62bee0d20dabb237d00caa65691
judgesupreme <1234surajsinghnegi@gmail.com>
2020-07-13 20:06:18 +0530

    last one

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -65,10 +65,9 @@ class Contact extends Component {
                     <div className="col-12 col-sm-11 offset-sm-1">
                         <div className="btn-group" role="group">
                             <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
-                            <a role="button" className="btn btn-info"><i className="fa fa-skype"></i> Skype</a>
                             <a role="button" className="btn btn-success" href="mailto:confusion@food.net"><i className="fa fa-envelope-o"></i> Email</a>
                         </div>
-                    </div>
+                    </div> 
                 </div>
                 <div className="row row-content">
                    <div className="col-12">
